5ccfb7b3a760c773045f1f31b54be0cb41935a03
Ayazhan <kuanysh.ayazhan070503@gmail.com>
2020-12-14 00:30:43 +0600

    abcdefg

--- lab10/a.cpp
+++ lab10/a.cpp
@@ -21,11 +21,8 @@ sort(v1.begin() , v1.end());
 sort(v2.begin() , v2.end());
 
 for(int i=0 ; i<v1.size() ; i++){
-    for(int j=0 ; j<v2.size() ; j++){
-        cout << v1[i] <<" "<< v2[j] << endl ;
+    cout << v1[i] <<" "<< v2[i] << endl ;
     }
-}
-
 
     return 0 ;
 }
