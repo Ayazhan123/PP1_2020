04fff33ad259e0959cf3887d2a6b79b026a74505
Ayazhan <kuanysh.ayazhan070503@gmail.com>
2020-12-13 23:41:06 +0600

    abcdefg

---
+++ lab10/a.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+
+vector <string> v1 ;
+vector <int> v2 ;
+int n;
+cin >> n;
+for(int i=0 ; i<n ; i++){
+    string a;
+    int b;
+    cin >> a >> b ;
+    v1.push_back(a);
+    v2.push_back(b);
+}
+sort(v1.begin() , v1.end());
+sort(v2.begin() , v2.end());
+
+for(int i=0 ; i<v1.size() ; i++){
+    for(int j=0 ; j<v2.size() ; j++){
+        cout << v1[i] <<" "<< v2[j] << endl ;
+    }
+}
+
+
+    return 0 ;
+}
